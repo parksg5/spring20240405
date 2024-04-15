39353bd33b99cf0e129029872ef1f3285a2a0062
parksg <park0110725@naver.com>
2024-04-15 17:17:01 +0900

    2024.04.15 오후수업

--- src/main/java/com/study/controller/Controller20.java
+++ src/main/java/com/study/controller/Controller20.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("main20")
@@ -77,7 +79,66 @@ public class Controller20 {
     // city, address, title, content 의 요청 파라미터가
     // MyBean202 객체의 property로 바인딩 되도록 코드 작성
     @RequestMapping("sub10")
-    public void sub10(@ModelAttribute MyBean202 data) {
-        System.out.println("data = " + data);
+    public void sub10(MyBean202 obj) {
+        System.out.println("obj = " + obj);
+
     }
+
+    @RequestMapping("sub11")
+    public void sub11() {
+    }
+
+    @RequestMapping("sub12")
+    public void sub12(String type,
+                      String content,
+                      String city) {
+        System.out.println("type = " + type);
+        System.out.println("content = " + content);
+        System.out.println("city = " + city);
+    }
+
+    @RequestMapping("sub13")
+    public void sub13(String city) {
+        System.out.println("city = " + city);
+    }
+
+    @RequestMapping("sub14")
+    public void sub14(@RequestParam("foods") String[] foods) {
+        // @RequestParam("foods") 생략 가능
+        System.out.println(Arrays.toString(foods));
+    }
+
+    // todo : sub11.jsp 에 form 요소 작성
+    //  submit 클릭시 /main20/sub15 로 전송
+    //  type, phone, book 요청 파라미터 전송
+    //  type : select & option 요소로
+    //  phone : radio button 으로
+    //  book  : checkbox
+    @RequestMapping("sub15")
+    public void sub15(String type,
+                      String phone,
+                      String[] book) {
+        System.out.println("type = " + type);
+        System.out.println("phone = " + phone);
+        System.out.println("book = " + Arrays.toString(book));
+    }
+
+    @RequestMapping("sub16")
+    public void sub16() {
+    }
+
+    @RequestMapping("sub17")
+    public void sub17(String id, String password, String describe) {
+        System.out.println("id = " + id);
+        System.out.println("password = " + password);
+        System.out.println("describe = " + describe);
+    }
+
+    @RequestMapping("sub18")
+    public void sub18(String name, String city, String address) {
+        System.out.println("name = " + name);
+        System.out.println("city = " + city);
+        System.out.println("address = " + address);
+    }
+
 }
