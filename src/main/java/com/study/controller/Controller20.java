c4597dc27cb47fa420b0d4213d78c3160a56f557
parksg <park0110725@naver.com>
2024-04-15 13:50:01 +0900

    2024.04.12오후 04.15오전수업

---
+++ src/main/java/com/study/controller/Controller20.java
@@ -0,0 +1,83 @@
+package com.study.controller;
+
+import com.study.domain.MyBean201;
+import com.study.domain.MyBean202;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+
+@Controller
+@RequestMapping("main20")
+public class Controller20 {
+    @RequestMapping("sub1")
+    public void sub1() {
+    }
+
+    @RequestMapping("sub2")
+    public void sub2() {
+    }
+
+    @RequestMapping("sub3")
+    public void sub3() {
+    }
+
+    @RequestMapping("sub4")
+    public void sub4() {
+    }
+
+    @RequestMapping("sub5")
+    public void sub5(String player, Model model) {
+        // 1. 요청 분석/가공
+        // 2. 비즈니스 로직 처리
+        // 3. 결과를 모델에 담고
+        model.addAttribute("result", player + "검색 결과");
+        // 4. 뷰로 forward(뷰 이름 리턴)
+
+    }
+
+    @RequestMapping("sub6")
+    public void sub6(String address, String type, Model model) {
+        model.addAttribute("result", address + "의 " + type + " 검색 결과");
+    }
+
+    @RequestMapping("sub7")
+    public void sub7(String name,
+                     Integer age,
+                     LocalDate birth,
+                     String address,
+                     String hobby,
+                     Model model) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        System.out.println("birth = " + birth);
+        System.out.println("address = " + address);
+        System.out.println("hobby = " + hobby);
+
+    }
+
+    @RequestMapping("sub8")
+    public void sub8(@ModelAttribute MyBean201 data) {
+        // @ModelAttribute 생략 가능
+        System.out.println(data);
+    }
+
+    @RequestMapping("sub9")
+    public void sub9() {
+        //todo: sub9.jsp 만들고
+        // city, address, title, content
+        // 의 요청 파라미터 입력받고 전송하는
+        // form 요소 작성
+        // 전송 위치 : /main20/sub10
+    }
+
+    // /main20/sub10 요청 경로에서 일하는 메소드 작성
+    // city, address, title, content 의 요청 파라미터가
+    // MyBean202 객체의 property로 바인딩 되도록 코드 작성
+    @RequestMapping("sub10")
+    public void sub10(@ModelAttribute MyBean202 data) {
+        System.out.println("data = " + data);
+    }
+}
--- src/main/webapp/WEB-INF/view/main16/main/sub2.jsp
+++ src/main/webapp/WEB-INF/view/main16/main/sub2.jsp
@@ -5,12 +5,12 @@
     <title>Title</title>
 </head>
 <body>
-<c:import url="navbar2.jsp"/>
-<c:import url="/../../../WEB-INF/fragment/navbar2.jsp"/>
+<c:import url="/WEB-INF/fragment/navbar2.jsp"/>
+<c:import url="../../../fragment/navbar2.jsp"/>
 <div>
     sub2 content
 </div>
-<c:import url="footer2.jsp"/>
-<c:import url="/../../../WEB-INF/fargment/footer2.jsp"/>
+<c:import url="/WEB-INF/fragment/footer2.jsp"/>
+<c:import url="../../../fragment/footer2.jsp"/>
 </body>
 </html>
